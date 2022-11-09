d0d4e4ddc5c7d1995175bdb9b97422a631a02094
Muhanad Al-Khafaji <Muhanad.Al-Khafaji@softwire.com>
2022-11-09 17:00:20 +0000

    Added feature to list transactions given input of name. Also fixed typo.

--- SupportBank.js
+++ SupportBank.js
@@ -4,16 +4,21 @@ var readlineSync = require('readline-sync');
 //var moment = require('moment');
 //moment().format();
 
+// Class
 class Account {
     constructor(name) {
         this.name = name;
         this.amount = 0;
+        // Array for transactions out of account
         this.out = [];
+        // Array for transactions into account
         this.in = [];
     }
 }
+// Initialise object
 let people = {};
 
+// Reading csv file
 stream = fs.createReadStream("./Transactions2014.csv");
 
 stream = stream.pipe(parse({ delimiter: ",", from_line: 2 }));
@@ -28,24 +33,39 @@ stream.on('data', function (row) {
     if (names.includes(row[2]) == false) {
         people[row[2]] = new Account(row[2]);
     }
+    // Update amounts for both accounts involved in transaction
     people[row[1]].amount -= parseFloat(row[4]);
     people[row[2]].amount += parseFloat(row[4]);
+    // Update transaction lists for both accounts
     let indices = [0, 3, 4];
-    people[row[2]].out.push(indices.map(x => row[x]));
+    people[row[2]].in.push(indices.map(x => row[x]));
     row[4] *= -1;
     people[row[1]].out.push(indices.map(x => row[x]));
 });
-
+// Actions after csv file is read
 stream.on('end', function () {
     while (true) {
+        // Ask user for prompt
         var response = readlineSync.question('Choose an option:\n');
         if (response == "List All") {
+            // List all names and associated account values
             let list = Object.values(people);
             for (let i = 0; i < list.length; i++) {
                 console.log(list[i].name + ": " + list[i].amount);
             }
-        } else if (response == "List" + "a") {
-            console.log("A");
+        } else if (response.substring(0, 5) == "List ") {
+            // Obtain name from input
+            let name = response.substring(5);
+            // Print all transactions in
+            let list = people[name].in;
+            for (let i = 0; i < list.length; i++) {
+                console.log(list[i]);
+            }
+            // Print all transactions out
+            list = people[name].out;
+            for (let i = 0; i < list.length; i++) {
+                console.log(list[i]);
+            }
         }
     }
 });
